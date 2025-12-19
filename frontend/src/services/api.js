72fea3e90e2e7e138d3e654806155c83dd814579
Abishek-KS6 <abisheksakthivel228@gmail.com>
2025-12-19 13:07:13 +0530

    Fix API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://homebuddy-7ybp.onrender.com'
+  baseURL: 'https://homebuddy-7ybp.onrender.com/api'
 });
 
 API.interceptors.request.use((req) => {
